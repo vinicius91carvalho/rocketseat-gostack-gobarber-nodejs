5a9adae446941844a5b14aba5f083d7bb4e4ab86
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2020-11-15 10:52:57 -0300

    Add validation to optionally sends password information when old_passward was informed

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -16,7 +16,11 @@ profileRouter.put(
             email: Joi.string().email().required(),
             old_password: Joi.string(),
             password: Joi.string(),
-            password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+            password_confirmation: Joi.string().when('old_password', {
+                is: Joi.string().exist(),
+                then: Joi.string().required().valid(Joi.ref('password')),
+                otherwise: Joi.string(),
+            }),
         },
     }),
     profileController.update,
